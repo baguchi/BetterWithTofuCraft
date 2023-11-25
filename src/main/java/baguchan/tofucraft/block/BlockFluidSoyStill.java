5f1d79c12841d8cdf04e72051375bcec4b0e410e
baguchan <shutux500@gmail.com>
2023-11-25 22:41:56 +0900

    prevent crash about soymilk

--- src/main/java/baguchan/tofucraft/block/BlockFluidSoyStill.java
+++ src/main/java/baguchan/tofucraft/block/BlockFluidSoyStill.java
@@ -1,11 +1,97 @@
 package baguchan.tofucraft.block;
 
+import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockFluidStill;
 import net.minecraft.core.block.material.Material;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.World;
 
 public class BlockFluidSoyStill extends BlockFluidStill {
 	public BlockFluidSoyStill(String name, int openIds, Material soymilkMaterial) {
 		super(name, openIds, soymilkMaterial);
+	}
+
+
+	@Override
+	public void onNeighborBlockChange(World world, int x, int y, int z, int blockId) {
+		this.checkForHarden(world, x, y, z);
+		if (blockId == Side.TOP.getId()) {
+			return;
+		}
+		int id = world.getBlockId(x, y, z);
+		if (world.getBlockId(x, y, z) == this.id) {
+			this.func_30004_j(world, x, y, z);
+		}
+	}
+
+	private void func_30004_j(World world, int i, int j, int k) {
+		int l = world.getBlockMetadata(i, j, k);
+		world.editingBlocks = true;
+		world.setBlockAndMetadata(i, j, k, ModBlocks.soymilk_flow.id, l);
+		world.markBlocksDirty(i, j, k, i, j, k);
+		world.scheduleBlockUpdate(i, j, k, ModBlocks.soymilk_flow.id, this.tickRate());
+		world.editingBlocks = false;
+	}
+
+	private void checkForHarden(World world, int x, int y, int z) {
+		int meta;
+		boolean flag;
+		if (world.getBlockId(x, y, z) != this.id) {
+			return;
+		}
+		if (this.blockMaterial == Material.lava) {
+			flag = false;
+			if (flag || world.getBlockMaterial(x, y, z - 1) == Material.water) {
+				flag = true;
+			}
+			if (flag || world.getBlockMaterial(x, y, z + 1) == Material.water) {
+				flag = true;
+			}
+			if (flag || world.getBlockMaterial(x - 1, y, z) == Material.water) {
+				flag = true;
+			}
+			if (flag || world.getBlockMaterial(x + 1, y, z) == Material.water) {
+				flag = true;
+			}
+			if (flag || world.getBlockMaterial(x, y + 1, z) == Material.water) {
+				flag = true;
+			}
+			if (flag) {
+				meta = world.getBlockMetadata(x, y, z);
+				if (meta == 0) {
+					world.setBlockWithNotify(x, y, z, Block.obsidian.id);
+				} else if (meta <= 2) {
+					world.setBlockWithNotify(x, y, z, Block.cobbleGranite.id);
+				} else if (meta <= 4) {
+					world.setBlockWithNotify(x, y, z, Block.cobbleStone.id);
+				} else {
+					world.setBlockWithNotify(x, y, z, Block.cobbleBasalt.id);
+				}
+				this.triggerLavaMixEffects(world, x, y, z);
+			}
+		}
+		if (this.blockMaterial == Material.water) {
+			flag = false;
+			if (flag || world.getBlockMaterial(x, y, z - 1) == Material.lava) {
+				flag = true;
+			}
+			if (flag || world.getBlockMaterial(x, y, z + 1) == Material.lava) {
+				flag = true;
+			}
+			if (flag || world.getBlockMaterial(x - 1, y, z) == Material.lava) {
+				flag = true;
+			}
+			if (flag || world.getBlockMaterial(x + 1, y, z) == Material.lava) {
+				flag = true;
+			}
+			if (flag || world.getBlockMaterial(x, y + 1, z) == Material.lava) {
+				flag = true;
+			}
+			if (flag && (meta = world.getBlockMetadata(x, y, z)) == 0) {
+				world.setBlockWithNotify(x, y, z, Block.cobbleLimestone.id);
+				this.triggerLavaMixEffects(world, x, y, z);
+			}
+		}
 	}
 	@Override
 	public int tickRate() {
--- src/main/java/baguchan/tofucraft/entity/ModelTofunian.java
+++ src/main/java/baguchan/tofucraft/entity/ModelTofunian.java
@@ -3,10 +3,8 @@ package baguchan.tofucraft.entity;
 import net.minecraft.client.render.model.Cube;
 import net.minecraft.core.entity.EntityLiving;
 import useless.dragonfly.helper.AnimationHelper;
-import useless.dragonfly.model.entity.AnimationState;
 import useless.dragonfly.model.entity.BenchEntityModel;
 import useless.dragonfly.model.entity.animation.Animation;
-import useless.dragonfly.model.entity.animation.AnimationData;
 
 import static baguchan.tofucraft.TofuCraft.MOD_ID;
 
@@ -27,19 +25,13 @@ public class ModelTofunian extends BenchEntityModel {
 		if (this.getIndexBones().containsKey("Head")) {
 			this.getIndexBones().get("Head").setRotationAngle((float) Math.toRadians(headPitch), (float) Math.toRadians(headYaw), 0);
 		}
-		Animation animation = AnimationHelper.getOrCreateEntityAnimation(MOD_ID, "tofunian.animation");
-		animateWalk(animation.getAnimations().get("walk"), limbSwing, limbYaw, 2.0F, 0.5F);
 		if (this.tofunian != null) {
+			Animation animation = AnimationHelper.getOrCreateEntityAnimation(MOD_ID, "tofunian.animation");
+			animateWalk(animation.getAnimations().get("walk"), limbSwing, limbYaw, 2.0F, 0.5F);
+
 			Animation sit = AnimationHelper.getOrCreateEntityAnimation(MOD_ID, "tofunian.animation");
 			animate(tofunian.sitAnimation, animation.getAnimations().get("sit_down"), ticksExisted, 1F);
 		}
-	}
-
-	protected void animate(AnimationState animationState, AnimationData animationData, float p_233388_, float p_233389_) {
-		animationState.updateTime(p_233388_, p_233389_);
-		animationState.ifStarted((p_233392_) -> {
-			AnimationHelper.animate(this, animationData, p_233392_.getAccumulatedTime(), 0.5F, this.VEC_ANIMATION);
-		});
 	}
 
 	public void setRotationAngle(Cube modelRenderer, Cube cube) {
