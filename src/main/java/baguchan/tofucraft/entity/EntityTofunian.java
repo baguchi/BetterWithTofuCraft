e1c7a2d2fc17e04c99eb627347ddab61ceacbae0
baguchan <shutux500@gmail.com>
2024-04-21 15:11:08 +0900

    no longer sit in the soymilk

--- src/main/java/baguchan/tofucraft/entity/EntityTofunian.java
+++ src/main/java/baguchan/tofucraft/entity/EntityTofunian.java
@@ -61,13 +61,13 @@ public class EntityTofunian extends EntityAnimal {
 
 	@Override
 	protected void updatePlayerActionState() {
-		if (!this.isSit()) {
+		if (!this.isSit() && (this.isInWater() || this.onGround)) {
 			super.updatePlayerActionState();
 			if (random.nextInt(400) == 0) {
 				this.setSit(true);
 			}
 		} else {
-			if (random.nextInt(400) == 0) {
+			if (random.nextInt(400) == 0 || (this.isInWater() || this.onGround)) {
 				this.setSit(false);
 			}
 		}
